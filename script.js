5511af3d2f6e352241c14c433f70a8fdcd4b3b36
Rajat Pathania <83996978+pathania0001@users.noreply.github.com>
2025-05-28 14:50:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
 //your JS code here. If required.
+const input  = document.getElementById("fname");
+input.onblur = function small(e) {
+	  let value = (e.target.value);
+	  // e.target.value = value.toUpperCase();
+	  //    return;
+	   let arrOfValues = value.trim().split(" ").map((str)=>{
+			let lower = str.toLowerCase();
+		  return lower.charAt(0).toUpperCase() + lower.split("").slice(1).join("");
+           })
+	e.target.value = arrOfValues.join(" ");
+}
